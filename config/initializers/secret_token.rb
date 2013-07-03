5735a667232136b70cf2a6be29d6ad316460aa29
Quan Sun <cfk.quan@gmail.com>
2013-07-04 08:46:19 +1000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FounderFlirting::Application.config.secret_token = '0724e6aa1b84d31b21694f19a2a32b9accb93c00e4573c2e6a1b2b70633d19e4e2ef74042f76d3063907762b530ee33d019ffbc58ce112ddbd09659dc067f12c'
