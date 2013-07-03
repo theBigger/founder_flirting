3bf986ceded15671c621eb793264760b58a9b019
whatever@whatever.com <whatever@whatever.com>
2013-07-04 09:57:26 +1000

    annotate

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
