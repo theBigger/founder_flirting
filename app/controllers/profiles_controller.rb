0935b46daf3746fac47ebe8fc7da9dd2db65af08
whatever@whatever.com <whatever@whatever.com>
2013-07-06 11:25:49 +1000

    allow users to modify profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,13 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_user!, :only => [:update]
+
+  def update
+    current_user.profile = params[:user][:profile]
+    current_user.save
+    
+    redirect_to profile_url(current_user)
+  end
+
   def index
     @users = User.page(params[:page])
   end
